ac7145694912bea43b15a729ea3db6be6f695c89
EugenTrifonov <trifonov-15@mail.ru>
2021-12-18 14:36:23 +0300

    Initial commit

---
+++ Unequal_characters/Task1_Tests/Task1_Tests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task1_Tests
+{
+    [TestClass]
+    public class Task1_Tests
+    {
+        [TestMethod]
+        [DataRow("abcdefa", 6)]
+        [DataRow("11", 1)]
+        [DataRow("asdfga", 5)]
+        [DataRow("", 0)]
+        public void PositiveGetTheLongestUnequalCharacterNumber(string input, int expectedLength)
+        {
+            Assert.AreEqual(expectedLength, Unequalcharacters.UnequalCharactersSequence.GetMaxNumberofUnequalCharacters(new string[] { input }));
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        [DataRow(new string[] { "dfsdf", "adasdada", "dsfsf" })]
+        public void GetTheLongestUnequalCharacterNumberIncorrectInput(string[] input)
+        {
+            Unequalcharacters.UnequalCharactersSequence.GetMaxNumberofUnequalCharacters(input);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        [DataRow(new string[] { })]
+        public void GetTheLongestUnequalCharacterNumberEmptyInput(string[] input)
+        {
+            Unequalcharacters.UnequalCharactersSequence.GetMaxNumberofUnequalCharacters(input);
+        }
+    }
+}
---
+++ Unequal_characters/Unequal_characters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unequalcharacters;
+
+namespace Unequal_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(UnequalCharactersSequence.GetMaxNumberofUnequalCharacters(args));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
