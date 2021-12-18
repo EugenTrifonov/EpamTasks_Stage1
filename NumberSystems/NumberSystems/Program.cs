5a04e1e8d6cd7576b4005fa29567bf66673204c7
EugenTrifonov <trifonov-15@mail.ru>
2021-12-18 14:37:09 +0300

    Initial commit

---
+++ NumberSystems/NumberSystems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Numbersystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(Numbers.ConvertNumber(args));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
